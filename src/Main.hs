df81a5cb1e9b48a52ee99e87977cf9f1b8c8fa97
Sam Griffin <sam.griffin@gmail.com>
2012-07-07 18:38:27 -0400

    grid can now be modified

--- src/Main.hs
+++ src/Main.hs
@@ -23,18 +23,16 @@ drawScreen mainstate = let
 		(xSize, ySize) = screenSize mainstate
 		[centerX, centerY] = map (\x -> div x 2) [xSize, ySize]
 		surface_ = surface mainstate
-		icons_ = icons mainstate
 		(px,py) = position mainstate
-		grid_ = grid mainstate
 		xIcons = div xSize (iconXSize + 1)
 		yIcons = div ySize (iconYSize + 1)
 		positions = [(x, y) | -- positions of icons 
 			x <- [px - div xIcons 2.. px + div xIcons 2], 
 			y <- [py - div yIcons 2.. py + div yIcons 2]]
 		clearScreen = SDL.fillRect surface_ Nothing (SDL.Pixel 0)
-		drawIcon (x,y) = case Map.member (x,y) grid_ of
+		drawIcon (x,y) = case hasIconAt mainstate (x,y) of
 			True -> do 
-				let drawIcon = Icon.getByID icons_ (grid_ Map.! (x,y)) 
+				let drawIcon = getIconAt mainstate (x,y)
 				SDL.blitSurface (Icon.pic drawIcon) Nothing surface_ . Just $ SDL.Rect
 					(centerX + (x - px) * (iconXSize + 1))
 					(centerY + (y - py) * (iconYSize + 1))
@@ -56,22 +54,56 @@ main = do
 quitHandler :: STM.TVar MainState -> IO ()
 quitHandler tvms = do
 	mainState <- STM.atomically . STM.readTVar $ tvms
+	let inputContext_ = inputContext mainState
 	e <- SDL.waitEvent
 	case e of
 		SDL.Quit -> return ()
-		SDL.KeyDown (SDL.Keysym k _ _) -> let
-				modPosition s f = s { position = f $ position s }
-				doDirectionKey f = do
-					oldMainState <- STM.atomically . STM.readTVar $ tvms
-					let newMainState = modPosition oldMainState f
-					drawScreen newMainState
-					STM.atomically $ STM.writeTVar tvms newMainState
-					quitHandler tvms
-			in case k of 
-				SDL.SDLK_UP -> doDirectionKey (\(x, y) -> (x, y - 1))
-				SDL.SDLK_DOWN -> doDirectionKey (\(x, y) -> (x, y + 1))
-				SDL.SDLK_RIGHT -> doDirectionKey (\(x, y) -> (x + 1, y))
-				SDL.SDLK_LEFT -> doDirectionKey (\(x, y) -> (x - 1, y))
-				SDL.SDLK_ESCAPE -> return ()
-				otherwise -> quitHandler tvms
+		SDL.KeyDown (SDL.Keysym k _ _) -> case inputContext_ of
+			NoContext -> let
+					modPosition s f = s { position = f $ position s }
+					doDirectionKey f = do
+						oldMainState <- STM.atomically . STM.readTVar $ tvms
+						let newMainState = modPosition oldMainState f
+						drawScreen newMainState
+						STM.atomically $ STM.writeTVar tvms newMainState
+						quitHandler tvms
+				in case k of 
+					SDL.SDLK_u -> doDirectionKey (\(x, y) -> (x, y - 1)) -- UP
+					SDL.SDLK_e-> doDirectionKey (\(x, y) -> (x, y + 1)) -- DOWN
+					SDL.SDLK_t -> doDirectionKey (\(x, y) -> (x + 1, y)) -- RIGHT
+					SDL.SDLK_h -> doDirectionKey (\(x, y) -> (x - 1, y)) -- LEFT
+					SDL.SDLK_i -> do -- INPUT
+						STM.atomically $ do
+							oldMainState <- STM.readTVar tvms
+							STM.writeTVar tvms oldMainState { inputContext = IsInsert }
+						quitHandler tvms
+					SDL.SDLK_ESCAPE -> return ()
+					otherwise -> quitHandler tvms
+			IsInsert ->	let
+					setNewIcon id = do
+						newMainState <- STM.atomically $ do
+							oldMainState <- STM.readTVar tvms
+							STM.writeTVar tvms $ setIconAtCursor id $ oldMainState { inputContext = NoContext }
+							STM.readTVar tvms
+						drawScreen newMainState
+						quitHandler tvms
+					cancelInput = do
+						STM.atomically $ do
+							oldMainState <- STM.readTVar tvms
+							STM.writeTVar tvms oldMainState { inputContext = NoContext }
+						quitHandler tvms
+				in case k of
+					SDL.SDLK_i -> do -- clear icon
+						newMainState <- STM.atomically $ do
+							oldMainState <- STM.readTVar tvms
+							STM.writeTVar tvms $ removeIconAtCursor $ oldMainState { inputContext = NoContext }
+							STM.readTVar tvms
+						drawScreen newMainState
+						quitHandler tvms
+					SDL.SDLK_u -> setNewIcon 1
+					SDL.SDLK_e -> setNewIcon 2
+					SDL.SDLK_ESCAPE -> cancelInput
+					otherwise -> cancelInput
+				
 		otherwise -> quitHandler tvms
+			
--- src/MainState.hs
+++ src/MainState.hs
@@ -1,7 +1,15 @@
 module MainState 
 	( MainState(..)
-	, initMainState
+	, InputContext(..)
 	, Point
+	, initMainState
+	, hasIconAt
+	, getIconAt
+	, getIconAtCursor
+	, setIconAt
+	, setIconAtCursor
+	, removeIconAt
+	, removeIconAtCursor
 	) where
 
 import Graphics.UI.SDL as SDL
@@ -14,12 +22,16 @@ startYSize = 480
 
 type Point = (Int, Int)
 
+data InputContext = IsInsert {
+} | NoContext
+
 data MainState = MainState {
    surface :: Surface,
    icons :: Icon.IconSet,
    position :: Point,
 	screenSize :: (Int, Int),
-   grid :: Map.Map Point Int
+   grid :: Map.Map Point Int,
+	inputContext :: InputContext
 }
 
 testInitMap = Map.fromList [((0,0), 1), ((0,1), 2), ((0,(-1)), 2)]
@@ -35,9 +47,33 @@ initMainState = do
 		(0,0)
 		(startXSize, startYSize)
 		testInitMap
+		NoContext
+
+hasIconAt :: MainState -> Point -> Bool
+hasIconAt mainState point = Map.member point $ grid mainState
 
 getIconAt :: MainState -> Point -> Icon.Icon
 getIconAt mainState point = let
 		_icons = icons mainState
 		_grid = grid mainState
 	in Icon.getByID _icons $ _grid Map.! point
+
+getIconAtCursor :: MainState -> Icon.Icon
+getIconAtCursor mainState = getIconAt mainState $ position mainState
+
+setIconAt :: Point -> Int -> MainState -> MainState 
+setIconAt point id mainState = let
+		grid_ = grid mainState
+		newGrid = Map.insert point id grid_
+	in mainState { grid = newGrid }
+	
+setIconAtCursor :: Int -> MainState -> MainState 
+setIconAtCursor id mainState = setIconAt (position mainState) id mainState 
+
+removeIconAt :: Point -> MainState -> MainState 
+removeIconAt point mainState = let
+		newGrid = Map.delete point (grid mainState) 
+	in mainState { grid = newGrid }
+
+removeIconAtCursor :: MainState -> MainState
+removeIconAtCursor mainState = removeIconAt (position mainState) mainState 
